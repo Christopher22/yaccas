eae2986948bf46322a7b15da7b588e46c2447c2c
Christopher Gundler <c.gundler@mail.de>
2016-12-22 13:07:55 +0100

    Replace macro

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,37 @@ unstable_features, unused_import_braces, unused_qualifications)]
 pub mod arguments;
 pub mod parser;
 pub mod scanner;
+
+/// This macro returns the appropriated `Scanner` for the current operation system.
+/// # Example
+/// ```
+/// #[macro_use]
+/// extern crate yaccas;
+/// use yaccas::parser::{Result, Parser};
+///
+/// fn main() {
+///     let mut parser = Parser::default();
+///     assert_eq!(parser.parse(default_scanner!()), Result::Success(vec![]));
+/// }
+/// ```
+#[macro_export]
+macro_rules! default_scanner {
+    () => ({
+            use yaccas::scanner::{Unix, Windows};
+            use std::env::Args;
+
+            #[cfg(target_family = "windows")]
+            #[inline(always)]
+            fn default() -> Windows<Args> {
+                Windows::<Args>::default()
+            }
+
+            #[cfg(not(target_family = "windows"))]
+            #[inline(always)]
+            fn default() -> Unix<Args> {
+                Unix::<Args>::default()
+            }
+
+            default()
+    })
+}
--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -9,37 +9,3 @@ pub use self::scanner::Scanner;
 pub use self::token::Token;
 pub use self::scanner_unix::Unix;
 pub use self::scanner_windows::Windows;
-
-/// This macro returns the appropriated `Scanner` for the current operation system.
-/// # Example
-/// ```
-/// #[macro_use]
-/// extern crate yaccas;
-/// use yaccas::parser::{Result, Parser};
-///
-/// fn main() {
-///     let mut parser = Parser::default();
-///     assert_eq!(parser.parse(default_scanner!()), Result::Success(vec![]));
-/// }
-/// ```
-#[macro_export]
-macro_rules! default_scanner {
-    () => ({
-            use yaccas::scanner::{Unix, Windows};
-            use std::env::Args;
-
-            #[cfg(target_family = "windows")]
-            #[inline(always)]
-            fn default() -> Windows<Args> {
-                Windows::<Args>::default()
-            }
-
-            #[cfg(not(target_family = "windows"))]
-            #[inline(always)]
-            fn default() -> Unix<Args> {
-                Unix::<Args>::default()
-            }
-
-            default()
-    })
-}
